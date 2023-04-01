02f8ae067858be741f8f6ebb580423652a4a9348
Jules Chevenet <74178881+ptitchev@users.noreply.github.com>
2023-04-01 17:41:31 +0200

    Add files via upload

---
+++ style/css.py
@@ -0,0 +1,13 @@
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """ #permet de cacher le burger menu
+
+hide_sidebar_style =  """
+    <style>
+        [data-testid="collapsedControl"] {
+            display: none
+        }
+    </style>
+    """ #permet de cacher le nav menu
