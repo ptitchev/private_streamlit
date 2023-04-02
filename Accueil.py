c87f984e08c01d16785a7a3483b6adf5a23b9498
Jules Chevenet <74178881+ptitchev@users.noreply.github.com>
2023-04-02 19:10:45 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import streamlit.components.v1 as components
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from github import Github
