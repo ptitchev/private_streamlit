48a22d8abdcc9f8e12036f28bc630486cc88f5ad
Jules Chevenet <74178881+ptitchev@users.noreply.github.com>
2023-04-06 20:15:17 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -198,19 +198,20 @@ if check_password():
                 if "code" not in st.experimental_get_query_params():
                     auth_url = sp_oauth.get_authorize_url()
                     st.write(f"[Débugger]({auth_url})")
-                sp = handle_spotify_callback()
-                search_query = st.text_input('Rechercher une musique sur Spotify')
-                if search_query:
-                    results = sp.search(q=search_query, type='track', limit=10)
-                    tracks = results["tracks"]["items"]
-                    for track in tracks:
-                        col1, col2 = st.columns([4,1])
-                        with col1:
-                            comp_musique(track["id"])
-                        with col2:
-                            st.write('')
-                            st.write('')
-                            st.button('Ajouter', key = track["id"], on_click=lambda track_id=track["id"]: add_s(track_id), disabled=check_track_in_playlist(track["id"]), use_container_width=True)
+                else :
+                    sp = handle_spotify_callback()
+                    search_query = st.text_input('Rechercher une musique sur Spotify')
+                    if search_query:
+                        results = sp.search(q=search_query, type='track', limit=10)
+                        tracks = results["tracks"]["items"]
+                        for track in tracks:
+                            col1, col2 = st.columns([4,1])
+                            with col1:
+                                comp_musique(track["id"])
+                            with col2:
+                                st.write('')
+                                st.write('')
+                                st.button('Ajouter', key = track["id"], on_click=lambda track_id=track["id"]: add_s(track_id), disabled=check_track_in_playlist(track["id"]), use_container_width=True)
         #Jeu
 
         with tab3 :
