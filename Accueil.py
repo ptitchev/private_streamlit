2306c903a2354f8112269e5b13c986f2577b28f6
Jules Chevenet <74178881+ptitchev@users.noreply.github.com>
2023-04-06 13:53:26 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -2,24 +2,25 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import streamlit.components.v1 as components
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from github import Github
 from PIL import Image
 import json
 from style.css import hide_menu_style, hide_sidebar_style
 from game_tools.game_engine import *
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 st.set_page_config(page_title="Projet Chev", initial_sidebar_state="collapsed") #configue page (Nome et nav menu fermé)
-#st.markdown(hide_menu_style, unsafe_allow_html=True) #applique hide_menu_style
+st.markdown(hide_menu_style, unsafe_allow_html=True) #applique hide_menu_style
 
 df_shown = pd.read_csv('https://raw.githubusercontent.com/ptitchev/private_streamlit/main/data/ds.csv')
 df_rep = pd.read_csv('https://raw.githubusercontent.com/ptitchev/private_streamlit/main/data/dr.csv')
 
-#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=st.secrets["client_id"],
-                                               #client_secret=st.secrets["client_secret"],
-                                               #redirect_uri='https://projet-chev.streamlit.app/',
-                                               #scope='playlist-modify-public'))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=st.secrets["client_id"],
+                                               client_secret=st.secrets["client_secret"],
+                                               redirect_uri='https://projet-chev.streamlit.app/callback',
+                                               scope='playlist-modify-public'))
+
 
 def commit(df, name):
     updated_content = df.to_csv(index=False)
@@ -41,21 +42,16 @@ def check_password():
             st.session_state["password_correct"] = True
             if st.session_state["password"] == st.secrets["passworda"]:
                 st.session_state["a"] = 1
-            del st.session_state["password"]
-            
+            del st.session_state["password"]           
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
         st.markdown(hide_sidebar_style,unsafe_allow_html=True)
-        st.text_input(
-            "Mot de passe", type="password", on_change=password_entered, key="password"
-        )
+        st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
         return False
     elif not st.session_state["password_correct"]:
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
+        st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
         st.error("Mauvais mot de passe BG, regarde ma bio insta")
         return False
     else:
@@ -67,8 +63,7 @@ def start_G():
     st.session_state["musique"] = 0
     if 'RS' in st.session_state :
         del st.session_state["RS"]
-        
-        
+
 def check_track_in_playlist(track_id):
     tracks = sp.playlist_tracks(playlist_id=st.secrets["playlist_id"], fields="items.track.id,total")
     for item in tracks['items']:
@@ -77,7 +72,7 @@ def check_track_in_playlist(track_id):
     return False
 
 def add_s(track_id):
-    sp.playlist_add_items(playlist_id, [track_id])
+    sp.playlist_add_items(st.secrets["playlist_id"], [track_id])
 
 def comp_musique(id):
     return components.html("""<iframe 
@@ -91,20 +86,15 @@ def comp_musique(id):
                     picture-in-picture" 
                     loading="lazy">
                     </iframe>""", height=92)
+
     
 if check_password():
 
     if "a" not in st.session_state:
         st.subheader("Bien joué mon reuf : tu es invité(e) à l'anniv de Chev")
-
         tab1, tab2, tab3, tab4, tab5, tab6  = st.tabs(["Présentation", "Participants", "Jeux", "Thème", "Musique", "Infos supplémentaires"])
 
-
-        #Présentation
-
         with tab1 :
-            
-
             st.markdown("### Informations")
             st.markdown("Comme chaque années depuis 2015, pour célébrer mon anniversaire, j'organise un **grand** **weekend** **festif**. Au programme, BBQ, piscine, basket, pétanque et autres activités, mais surtout beaucoup d'**alcool** (même si c'est pas cool) et de rires.")
             st.markdown("J'avoue ça a l'air barbant comme ça, mais pour voir encore plus grand, je m'organise en avance pour nous permettre de mieux festoyer. Si t'es chaud, on se retrouve en **région** **maconnaise** pour fêter ça.")
@@ -113,12 +103,9 @@ if check_password():
             st.markdown("**Chev**")
 
 
-        #Participation
-
         with tab2:
-            
-            st.warning("Remplis l'inscription si ton nom n'apparait pas", icon="⚠️")
             st.dataframe(df_shown, use_container_width= True)
+            st.warning("Remplis l'inscription si ton nom n'apparait pas", icon="⚠️")
             with st.expander("Inscription"):
                 with st.form("Inscription", clear_on_submit = True):
                     st.write('Remplis-moi ça :')
@@ -153,7 +140,6 @@ if check_password():
         #Theme
 
         with tab4:
-            
             col1, col2, col3 = st.columns(3)
             st.info("Reviens le 02/05 pour découvrir le thème")
             with col2:
@@ -169,15 +155,11 @@ if check_password():
                 st.write("2406 route de la Grisière, 71870, HURIGNY")
                 st.write("Gares les plus proches : Mâcon Loché TGV ou Mâcon Ville")
                 st.write("Afin de limiter l'impact environnemental de l'évènement, vous pouvez également faire du covoiturage avec les autres participants.")
-
             with st.expander("Principe du Weekend"):
                 st.write("Durant l'ensemble du Weekend, il y aura différents évènements en équipe ou en solo, avec des goodies à la clé, c'est pour ça qu'il y a un logo.")
                 st.write("Merci à toi de lire ces lignes, la curiosité et la réactivité vis à vis de ce site peuvent être utile pour prendre une longueur d'avance sur les autres. Joue-la comme Hercule Poirot.")
 
-
-        #Jeu
         with tab5 :
-
             components.html("""<iframe 
                                 style="border-radius:12px" 
                                 src="https://open.spotify.com/embed/playlist/0n3S3n3mroDR8ffyW9CTEJ?utm_source=generator&theme=0" 
@@ -192,32 +174,30 @@ if check_password():
                                 picture-in-picture" 
                                 loading="lazy">
                                 </iframe>""", height=164)
+            with st.expander('Ajouter des musiques'):
+                search_query = st.text_input('Rechercher une musique sur Spotify')
+                if search_query:
+                    results = sp.search(q=search_query, type='track', limit=10)
+                    tracks = results["tracks"]["items"]
+                    for track in tracks:
+                        col1, col2 = st.columns([4,1])
+                        with col1:
+                            comp_musique(track["id"])
+                        with col2:
+                            st.write('')
+                            st.write('')
+                            st.button('Ajouter', key = track["id"], on_click=lambda track_id=track["id"]: add_s(track_id), disabled=check_track_in_playlist(track["id"]), use_container_width=True)
+        #Jeu
 
-            #with st.expander('Ajouter des musiques'):
-                #search_query = st.text_input('Rechercher une musique sur Spotify')
-                #if search_query:
-                #results = sp.search(q='Disiz', type='track', limit=10)
-                #tracks = results["tracks"]["items"]
-                #for track in tracks:
-                    #col1, col2 = st.columns([4,1])
-                    #with col1:
-                        #comp_musique(track["id"])
-                    #with col2:
-                        #st.write('')
-                        #st.write('')
-                        #st.button('Ajouter', key = track["id"], on_click=lambda track_id=track["id"]: add_s(track_id), disabled=check_track_in_playlist(track["id"]), use_container_width=True)
         with tab3 :
-
             if "is_playing" not in st.session_state:
                 st.info('Sur téléphone, il est conseillé de passer en mode paysage sans les images.')
-
             c1, e1, c2 = st.columns([4,2,2])
             with c1:
                 st.markdown("### Jouer à : :violet[Party's Survivor]")#st.subheader("Jouer à : :violet[Party's Survivor]")
             with c2 :
                 blank1 = st.write("")
                 im = st.checkbox('Afficher image', value = False)
-            
             if "is_playing" not in st.session_state:
                 blank1 = st.write("")
                 blank2 = st.write("")
@@ -228,9 +208,7 @@ if check_password():
                 blank1 = st.write("")
                 blank2 = st.write("")
                 blank3 = st.write("")
-
             if "is_playing" in st.session_state :
-
                 if len(st.session_state["trois_choix"]) > 2 :
                     c1, c2, c3, c4 = st.columns([2,2,3,1])
                     with c1 :
@@ -281,7 +259,6 @@ if check_password():
                         Fun = st.progress(bar_val(st.session_state["game"],7), text="Fun")
 
                 else :
-                    
                     ddd = show_death(st.session_state["game"], st.session_state["trois_choix"])
                     col1,e1,col2,col3 = st.columns([6,2,2,2])
                     with col1:
@@ -339,10 +316,7 @@ if check_password():
                     with col2:
                         st.write('')
                         st.write('')
-                        
                         df = pd.DataFrame(m_score, columns = dscore['columns'], index = m_champ)
-
-
                         st.dataframe(df, use_container_width = True)
                         st.write("")
                         st.write("")
@@ -360,18 +334,15 @@ if check_password():
                 st.markdown("Premièrement, il faut essayer de survivre à la soirée. Plus la soirée est cool, plus tu marques de point")
                 st.markdown("A la fin de la partie, un score est attribué.")
                 st.markdown("Que le meilleur gagne.")
-                
 
 
 
     #Admin
     elif st.session_state["a"]==1:
-
         edited_ds = st.experimental_data_editor(df_shown, num_rows="dynamic")
         def modif_a1(df = edited_ds):
             commit(df, "ds")
-        st.button("Valider 1", on_click = modif_a1)
-                                                
+        st.button("Valider 1", on_click = modif_a1)                                    
         edited_dr = st.experimental_data_editor(df_rep, num_rows="dynamic")
         def modif_a2(df = edited_dr):
             commit(df, "dr")
